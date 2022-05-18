dea0647db410acecfad6228ef393bf9529bedbd8
FullStackKevin <joykevinrobin1@gmail.com>
2022-05-18 10:19:50 +0200

    good

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import RestProfile from './components/restaurant/RestProfile';
 import Orders from './components/Orders/Orders';
+import Menu from './components/Menus/menu'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<RestProfile />} />
           <Route path='/dashboard' element={<Orders />} />
+          <Route path='/menu' element={<Menu />} />
         </Routes>
       </BrowserRouter>
     </div>
