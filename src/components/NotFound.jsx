cb5981d2e037bfdaf58e644e903e67227dbf1465
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-05-20 08:24:23 +0200

    still

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios  from "axios";
 
 function NotFound() {
+
+const options = {
+  method: 'GET',
+  url: 'https://bing-image-search1.p.rapidapi.com/images/search',
+  params: {q: 'skol'},
+  headers: {
+    'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'bbce629d3cmsh48cb41094daa35cp1157cejsn05466969482c'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
   return (
     <>
       <div className="h-[100vh] flex flex-col items-center justify-center w-full">
