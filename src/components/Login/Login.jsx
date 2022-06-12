14f48eb5e59cb1de63047f10d67979ddfc8d62bd
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-12 15:45:32 +0200

    revised login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,81 +2,87 @@ import React, { useState } from "react";
 import "./second-form.css";
 import food from "./images/food.png";
 import { api } from "../contexts/AppContext";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Login() {
-  const [info, setInfo] = useState({
-    login: "",
-    password: "",
-  });
-  const [status, setStatus] = useState('')
+	const [info, setInfo] = useState({
+		login: "",
+		password: "",
+	});
+	const [status, setStatus] = useState("");
 
+	const login = async (e) => {
+		e.preventDefault();
+		console.log(info);
+		try {
+			const res = await api.post("/auth/signin", info);
+			console.log(res.data);
+			if (res.data.token) {
+				localStorage.setItem("token", JSON.stringify(res.data.token));
+				window.location.replace("http://localhost:3000/dashboard/overview");
+			}
+		} catch (error) {
+			if (error.response.data.apierror.message) {
+				console.log(error.response.data.apierror.message);
+				setStatus(error.response.data.apierror.message);
+			}
+		}
+		c;
+	};
 
-  const login = async (e) => {
-    e.preventDefault();
-    console.log(info);
-    const res = await api.post("/auth/signin", info);
-    console.log(res.data);
-    if (res.data.token) {
-      localStorage.setItem("token", JSON.stringify(res.data.token));
-      window.location.replace("http://localhost:3000/dashboard/overview");
-    }
-    
-  };
-
-  return (
-    <div className="h-screen w-full">
-      <div className="parents mx-auto">
-        <div className="mains">
-          <div className="childs">
-            <div className="doubles">
-              <div className="images ml-3">
-                <p className="text-3xl">
-                  Lean<span className="text-green-800">Taste</span>
-                </p>
-                <p className="mt-2 text">Here to eat Enjoy the food</p>
-
-                <img src={food} alt="" />
-              </div>
-
-              <div className="formss">
-                <h3 className="h3 text-lg font-bold text-center mt-16">Welcome back!</h3>
-                <p className="p1 mt-8">Your presence is what we are asking for to make it a memorable one! </p>
-
-                {/* <p></p> */}
-                <form action="" className="si-form" onSubmit={login}>
-                  E-mail
-                  <input
-                    onClick={(e) => setInfo({ ...info, login: e.target.value })}
-                    className="px-3 focus:border-green-800 outline-none mt-5 rounded-3xl"
-                    type="text"
-                    name="email"
-                  />
-                  <br />
-                  Password
-                  <input
-                    onChange={(e) =>
-                      setInfo({ ...info, password: e.target.value })
-                    }
-                    className="px-3 focus:border-green-800 outline-none mt-5 rounded-3xl"
-                    type="password"
-                    name="password"
-                  />
-                  <br />
-                  <button type="submit" className="bg-green-800 mt-10">
-                    Login
-                  </button>
-                  <p className="mt-10 ml-28">
-                    Don't have an account? <Link to='/signup'>Sign</Link>
-                  </p>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full h-screen flex flex-col">
+			<p className="text-3xl mt-5 ml-6">
+				Lean<span className="text-green-800">Taste</span>
+			</p>
+			<div className="flex h-full px-3 w-full">
+				<div className="flex flex-col w-1/2 h-full items-center justify-center">
+					<img className="w-[200px]" src="./src/food.png" alt="food" />
+					<p className="text-3xl text-center">
+						Join LeanTaste and find a thousand of restaurant and order your
+						favorite food
+					</p>
+				</div>
+				<div className="flex flex-col w-1/2 h-full items-center justify-center">
+					<form
+						onSubmit={login}
+						className="rounded-2xl text-xl min-w-[280px] items-center p-4 bg-opacity-10 text- text-black bg-slate-900 flex flex-col w-2/3"
+					>
+						<h1 className="text-2xl">Login</h1>
+						<p className="text-red-500">{status}</p>
+						<div className="flex flex-col mt-4 w-full px-[2%]">
+							<label htmlFor="">E-mail</label>
+							<input
+								onChange={(e) => setInfo({ ...info, login: e.target.value })}
+								type="text"
+								className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[50px]"
+							/>
+						</div>
+						<div className="flex flex-col mt-4 w-full px-[2%]">
+							<label htmlFor="">Password</label>
+							<input
+								onChange={(e) => setInfo({ ...info, password: e.target.value })}
+								type="password"
+								className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[50px]"
+							/>
+						</div>
+						<div className="flex flex-col mt-6 w-1/2 px-[2%] text-white">
+							<input
+								type="submit"
+								className="w-full cursor-pointer px-2 outline-none bg-green-900 h-[40px]"
+							/>
+						</div>
+						<div className="flex w-full justify-center mt-5">
+							<p>Don't have an account?</p>
+							<Link className="text-green-900 ml-3 font-semibold" to="/signup">
+								Sign Up
+							</Link>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
--- src/components/Login/form.jsx
+++ src/components/Login/form.jsx
@@ -6,128 +6,88 @@ import food from "./images/food.png";
 // import logo from '../Images/logogreen.pnp'
 
 const Form = () => {
-  const [info, setInfo] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    mobile: "",
-    password: "",
-  });
-  const [status, setStatus] = useState('')
+	const [info, setInfo] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		mobile: "",
+		password: "",
+	});
+	const [status, setStatus] = useState("");
 
-  const register = async (e) => {
-    e.preventDefault();
-    console.log(info);
-    try {
-      const res = await api.post("/auth/admin/signup", info);
-      console.log(res.data);
-      if (res.data.success === true) {
-        window.location.replace("http://localhost:3000/login");
-      }
-    } catch (error) {
-      if (error.response.data.apierror.message) {
-        console.log(error.response.data.apierror.message);
-        setStatus(error.response.data.apierror.message)
-      }
-    }
-  };
+	const register = async (e) => {
+		e.preventDefault();
+		console.log(info);
+		try {
+			const res = await api.post("/auth/admin/signup", info);
+			console.log(res.data);
+			if (res.data.success === true) {
+				window.location.replace("http://localhost:3000/login");
+			}
+		} catch (error) {
+			if (error.response.data.apierror.message) {
+				console.log(error.response.data.apierror.message);
+				setStatus(error.response.data.apierror.message);
+			}
+		}
+	};
 
-  return (
-    <div>
-        <div className="main">
-          <div className="child">
-            <div className="double">
-              <div className="image ml-3">
-                <p className="text-3xl">
-                  Lean<span className="text-green-800">Taste</span>
-                </p>
-                <p>Here to eat Enjoy the food</p>
-
-                <img src={food} alt="" />
+	return (
+		<div className="w-full h-screen flex flex-col">
+			<p className="text-3xl mt-5 ml-6">
+				Lean<span className="text-green-800">Taste</span>
+			</p>
+			<div className="flex h-full px-3 w-full">
+				<div className="flex flex-col w-1/2 h-full translate-y-[-100px] items-center justify-center">
+          <img className="w-[200px]" src="./src/food.png" alt="food" />
+					<p className="text-3xl text-center">Join LeanTaste and find  a thousand of restaurant and order your favorite food</p>
+				</div>
+				<div className="flex flex-col w-1/2 h-full items-center justify-center">
+          <form onSubmit={register} className="rounded-2xl min-w-[280px] text-xl items-center p-4 bg-opacity-10 text- text-black bg-slate-900 flex flex-col w-2/3">
+              <h1 className="text-2xl">Signup</h1>
+              <p className="text-red-500">{status}</p>
+              <div className="flex flex-col mt-4 w-full px-[2%]">
+                <label htmlFor="">FirstName</label>
+                <input 
+                  onChange={(e)=> setInfo({...info, firstName: e.target.value})}
+                type="text"  className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[45px]" />
               </div>
-
-              <div className="forms">
-                <form action="" className=" h-[90%]"  onSubmit={register}>
-                  <h3 className="text-2xl font-lg">Sign Up</h3>
-                  <div className="w-4/5 flex justify-around">
-                  <h2>Firstname:</h2>
-                  <input
-                    onChange={(e) =>
-                      setInfo({ ...info, firstName: e.target.value })
-                    }
-                    className="px-3 focus:border-2 border-green-800
-                   outline-none"
-                    type="text"
-                    name="firstname"
-                  />
-                  </div>
-                  
-                    <div className="w-4/5 flex justify-around">
-                  <h2>LastName</h2>
-                  <input
-                    onChange={(e) =>
-                      setInfo({ ...info, lastName: e.target.value })
-                    }
-                    className="px-3 focus:border-2 border-green-800
-                   outline-none"
-                    type="text"
-                    name="lastname"
-                  />
-                  </div>
-                    <div className="w-4/5 flex justify-around">
-                 <h2>E-mail</h2>
-                 
-                  <input
-                    onChange={(e) => setInfo({ ...info, email: e.target.value })}
-                    className="px-3 focus:border-2 border-green-800
-                   outline-none"
-                    type="text"
-                    name="email"
-                  />
-                  </div>
-                    <div className="w-4/5 flex justify-around">
-                  <h2>Phone</h2>
-                  
-                  <input
-                    onChange={(e) =>
-                      setInfo({ ...info, mobile: e.target.value })
-                    }
-                    className="px-3 focus:border-2 border-green-800
-                   outline-none"
-                    type="number"
-                    name="phone"
-                  />
-                  </div>
-                    <div className="w-4/5 flex justify-around">
-                  <h2>Password</h2>
-                  <input
-                    onChange={(e) =>
-                      setInfo({ ...info, password: e.target.value })
-                    }
-                    className="px-3 focus:border-2 border-green-800
-                   outline-none"
-                    type="password"
-                    name="password"
-                  />
-                  </div>
-                  <button className="button text-green-800" type="submit">
-                    Sign Up
-                  </button>
-                  <p className="ml-40">
-                    Already have an account?{" "}
-                    <Link
-                      to="/login"
-                      className="text-green-800"
-                    >
-                      Login
-                    </Link>
-                  </p>
-                </form>
+              <div className="flex flex-col mt-4 w-full px-[2%]">
+                <label htmlFor="">LastName</label>
+                <input
+                onChange={(e)=> setInfo({...info, lastName: e.target.value})}
+                 type="text"  className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[45px]" />
               </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  );
+              <div className="flex flex-col mt-4 w-full px-[2%]">
+                <label htmlFor="">E-mail</label>
+                <input
+                  onChange={(e)=> setInfo({...info, email: e.target.value})}
+                 type="text"  className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[45px]" />
+              </div>
+              <div className="flex flex-col mt-4 w-full px-[2%]">
+                <label htmlFor="">Phone</label>
+                <input
+                  onChange={(e)=> setInfo({...info, mobile: e.target.value})}
+                 type="text"  className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[45px]" />
+              </div>
+              <div className="flex flex-col mt-4 w-full px-[2%]">
+                <label
+                 htmlFor="">Password</label>
+                <input
+                 onChange={(e)=> setInfo({...info, password: e.target.value})}
+                 type="password"  className="w-full px-2 outline-none bg-transparent border-2 border-black rounded-xl h-[45px]" />
+              </div>
+              <div className="flex flex-col mt-4 w-1/2 px-[2%] text-white">
+                <input type="submit"  className="w-full cursor-pointer px-2 outline-none bg-green-900 h-[45px]" />
+              </div>
+              <div className="flex w-full justify-center mt-5">
+                <p>Already have an account?</p>
+                <Link className="text-green-900 ml-3 font-semibold" to='/login'>Login</Link>
+              </div>
+          </form>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Form;
