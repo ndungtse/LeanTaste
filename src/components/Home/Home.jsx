4847351a8a0701c5ca17b03dc5fd92069ee90b9e
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-12 23:07:53 +0200

    last of sunday

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ import "./home.css";
 
 function Home() {
 	return (
-		<div className="w-full home text-white flex flex-col p-6 bg-[#2b2a2a]">
-			<div className="flex flex-col">
+		<div className="w-full home text-white flex flex-col bg-[#2b2a2a]">
+			<div className="flex p-6 flex-col">
 				<img className="w-[20%] min-w-[150px] " src={logo} alt="" />
 				<p className="mt-2">Here to eat? Enjoy the food</p>
 			</div>
@@ -40,37 +40,63 @@ function Home() {
 				</Link>
 			</div>
 			<h1 className="text-3xl text-center mt-3">How it works</h1>
-			<div className="grid card grid-cols-3  gap-4 px-[5%] mt-2">
-				<div className=" bg-white h-[40vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
+			<div className="grid card grid-cols-3  gap-9 px-[5%] mt-6">
+				<div className=" bg-white h-[37vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
 					<img className="w-[120px]" src={reg} alt="" />
 					<h1 className="text-center text-2xl font-semibold mt-2">Step 1</h1>
 					<p className="text-1xl text-center mt-3">Register your restaurant</p>
 				</div>
-				<div className=" bg-white h-[40vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
+				<div className=" bg-white h-[37vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
 					<img className="w-[120px]" src={hotel} alt="" />
 					<h1 className="text-center text-2xl font-semibold mt-2">Step 2</h1>
 					<p className="text-1xl text-center mt-3">
 						Create your restaurant Profile
 					</p>
 				</div>
-				<div className=" bg-white h-[40vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
+				<div className=" bg-white h-[37vh]  text-black rounded-lg flex w-full flex-col items-center justify-center">
 					<img className="w-[120px]" src={good} alt="" />
 					<h1 className="text-center text-2xl font-semibold mt-2">Step 3</h1>
 					<p className="text-1xl text-center mt-3">Start receiving orders</p>
 				</div>
 			</div>
 			<div className="flex flex-col w-full mt-4">
-                <h1 className="text-center font-bold text-2xl">Don't leave your phone!</h1>
+                <h1 className="text-center font-bold text-4xl">Don't leave your phone!</h1>
 				<div className="flex w-full mt-4">
 					<img src="src/images/mobile.png" alt="" />
-                    <div className="flex flex-col w-1/2">
-                        <h1 className="text-2xl font-semibold">Introducing LeanTaste mobile App</h1>
-                        <p>You can now use your mobile app to order food
+                    <div className="flex flex-col w-1/2 mt-5 text-xl">
+                        <h1 className="text-3xl font-semibold">Introducing LeanTaste mobile App</h1>
+                        <p className="mt-5">You can now use your mobile app to order food
                              or create your own restaurant without leaving your phone.
+                             Just make sure you know what you want. This app is available on all platforms whether you
+                             are using iPhone or android
                              </p>
                     </div>
 				</div>
 			</div>
+            <div className="flex px-[10%] h-[30vh] w-full mt-7 bg-[#0000004f] justify-between">
+                <div className="flex p-3 flex-col">
+			    	<img className="w-[20%] min-w-[150px] " src={logo} alt="" />
+			    	<p className="mt-2">Here to eat? Enjoy the food</p>
+			    </div>
+                <div className="flex mt-3 flex-col">
+                    <h1 className="text-xl font-semibold">About Us</h1>
+                    <p className="mt-5">Smart Ordering</p>
+                    <p>Restaurant Registering</p>
+                    <p>Home delivery</p>
+                </div>
+                <div className="flex mt-3 flex-col">
+                    <h1 className="text-xl font-semibold">Features</h1>
+                    <p className="mt-5">Smart Ordering</p>
+                    <p>Restaurant Registering</p>
+                    <p>Home delivery</p>
+                </div>
+                <div className="flex mt-3 flex-col">
+                    <h1 className="text-xl font-semibold">Contact Us</h1>
+                    <p className="mt-5">Facebook</p>
+                    <p>Instagram</p>
+                    <p>+250 78123456</p>
+                </div>
+            </div>
 		</div>
 	);
 }
