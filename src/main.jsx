47b2d8771de7f34d72ed345c258eb50443fd1232
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-06-13 14:22:43 +0200

    ubuntu trial

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AppProvider } from './components/contexts/AppContext'
+// import ErrorBoundary from './Error';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    {/* <ErrorBoundary> */}
+      <AppProvider>
+        <App />
+      </AppProvider>
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 )
